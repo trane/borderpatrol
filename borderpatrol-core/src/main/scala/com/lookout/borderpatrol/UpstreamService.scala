875b4aa80c959e057b3a7d0425371ba54a1e4926
Will Kimeria <william.kimeria@lookout.com>
2014-12-30 12:14:40 -0800

    Using Dtab to re-write url (hard coded for now)

--- borderpatrol-core/src/main/scala/com/lookout/borderpatrol/UpstreamService.scala
+++ borderpatrol-core/src/main/scala/com/lookout/borderpatrol/UpstreamService.scala
@@ -1,15 +1,17 @@
 package com.lookout.borderpatrol
 
-<<<<<<< HEAD
 import com.lookout.borderpatrol.BorderPatrolApp.{NeedsAuthResponse, Response}
-=======
->>>>>>> de0f14b... Implement mock login/auth/token services
 import com.twitter.finagle.{Http, Service}
 import com.lookout.borderpatrol.BorderPatrolApp.{RoutedRequest, NeedsAuthResponse, Response}
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.{Request => FinagleRequest, Response => FinagleResponse}
+import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.loadbalancer.{HeapBalancerFactory, DefaultBalancerFactory}
+import com.twitter.finagle.service.SingletonFactory
+import com.twitter.finagle.{Dtab, Http, Service, Path}
+import com.twitter.finagle.http.{Request => FinagleRequest, Response => FinagleResponse, Http => FinagleHttp}
 import com.twitter.util.{Await, Future}
-import org.jboss.netty.handler.codec.http._
+import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse, HttpResponseStatus}
 
 /**
  * Generic upstream service
@@ -18,8 +20,20 @@ import org.jboss.netty.handler.codec.http._
 class UpstreamService(authService: Service[RoutedRequest, HttpResponse]) extends Service[HttpRequest, FinagleResponse] {
   def apply(request: HttpRequest) = {
     println("------------------------------ UpstreamService " + request.getUri + "----------------------------->")
-    //val resp = Http.fetchUrl("https://localhost:8081/mtp" + request.getUri)
-    val r = Http.fetchUrl("https://localhost:8081/mtp") map { resp => //TODO: This needs to be the appropriate HTTP Verb
+
+    val mappedUrl = getRewrittenUrl("/mtp/good")
+    println("MAPPED URL **********************> " + mappedUrl)
+
+    //TODO: Figure out the correct way of load balancing
+    val client = ClientBuilder()
+      .codec(FinagleHttp())
+      .hosts("localhost:8081,localhost:8082")
+      .hostConnectionLimit(1)
+      .loadBalancer(HeapBalancerFactory.toWeighted)
+      .retries(2)
+      .build()
+
+    val r = client(request) map { resp =>
       resp.getStatus match {
         case HttpResponseStatus.UNAUTHORIZED => {
           println("returning a 401")
@@ -30,5 +44,10 @@ class UpstreamService(authService: Service[RoutedRequest, HttpResponse]) extends
     }
     println("<----------------------------- UpstreamService ------------------------------")
     r
+  }
+
+  def getRewrittenUrl(url:String):String = {
+    val dTab: Dtab = Dtab.read("/mtp=>/")
+    dTab.lookup(Path.read(url)).sample().show
   }
 }
