8ad6e65e7d7662143bbc257bc602ae8ef59232e5
Andrew Kuhnhausen <andrew.kuhnhausen@lookout.com>
2015-01-05 15:48:08 -0800

    Add loginfilter tests

---
+++ borderpatrol-core/src/test/scala/com/lookout/borderpatrol/LoginFilterSpec.scala
@@ -0,0 +1,41 @@
+package com.lookout.borderpatrol
+
+import com.lookout.borderpatrol.session.{EmptyToken, Session}
+import com.twitter.util.Await
+import org.jboss.netty.handler.codec.http._
+import org.scalatest.{Matchers, FlatSpec}
+import com.twitter.finagle.http.{Cookie, Response => FinagleResponse}
+
+class LoginFilterSpec extends FlatSpec with Matchers {
+  def loginReq = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/a/login")
+  def httpReq = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/a")
+  def httpRep = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+  val serviceName = "foo"
+  def mockLoginRequest = RoutedRequest(loginReq, serviceName)
+  def mockRequest = RoutedRequest(httpReq, "bar")
+  def mockResponse = FinagleResponse(httpRep)
+  def mockCookie(value: String): Cookie = new Cookie(Session.cookieName, value)
+  val mockTokenService = new TokenService
+  val mockLoginService = new LoginService
+  def filter = new LoginFilter
+
+  behavior of "LoginFilter"
+
+  it should "parse responses from Login requests" in {
+    val resp = filter(mockLoginRequest, mockTokenService)
+    Await.result(resp) shouldBe a [TokenResponse]
+  }
+
+  it should "contain tokens from valid Login requests" in {
+    val resp = filter(mockLoginRequest, mockTokenService)
+    Await.result(resp) match {
+      case TokenResponse(req) => (req.serviceToken getOrElse EmptyToken) should not be EmptyToken
+      case _ => fail("Not a token response")
+    }
+  }
+
+  it should "create a login response for all other requests" in {
+    val resp = filter(mockRequest, mockLoginService)
+    Await.result(resp) shouldBe a [LoginResponse]
+  }
+}
