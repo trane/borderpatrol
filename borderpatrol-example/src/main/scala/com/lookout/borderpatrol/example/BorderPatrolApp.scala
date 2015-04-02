209bc51771fb971aac180d041758758f416fbb6d
Andrew Kuhnhausen <andrew.kuhnhausen@lookout.com>
2015-04-02 13:07:34 -0700

    Add basic auth implementation example app
    This utilizes the new pluggable auth module to show how a basic auth
    service/filter would be created.

--- borderpatrol-example/src/main/scala/com/lookout/borderpatrol/example/BorderPatrolApp.scala
+++ borderpatrol-example/src/main/scala/com/lookout/borderpatrol/example/BorderPatrolApp.scala
@@ -1,14 +1,39 @@
 package com.lookout.borderpatrol.example
 
-import java.net.InetSocketAddress
-
+import com.lookout.borderpatrol.util.Combinators.tap
+import com.lookout.borderpatrol.auth._
+import BasicAuth._
 import com.lookout.borderpatrol._
-import com.twitter.finagle.Http
+import com.twitter.finagle.httpx.path._
 import com.twitter.finagle.httpx.service.RoutingService
-import com.twitter.finagle.httpx.{Request, Response, HttpMuxer}
+
+import com.twitter.finagle.{Service, httpx}
+import com.twitter.io.Charsets
 import com.twitter.server.TwitterServer
-import com.twitter.util.Await
+import com.twitter.util.{Base64StringEncoder, Await}
 
 object BorderPatrolApp extends TwitterServer {
+
+  val basicAuth = BasicAuthFilter
+
+  val basicAuthService = new Service[BorderRequest[Basic], httpx.Response] {
+    def apply(request: BorderRequest[Basic]) = {
+      val cred = request.authInfo.info.credential.get
+      val body = s"You have authenticated with $cred"
+      println(body)
+      tap(httpx.Response(httpx.Status.Ok))(r => r.contentString = body).toFuture
+    }
+  }
+
+  val backend = RoutingService.byPathObject {
+    case Root / "hello" => basicAuth andThen basicAuthService
+  }
+
+  def testExample(u: String, p: String) = {
+    val req = httpx.Request(httpx.Method.Get, "/hello")
+    val auth = Base64StringEncoder.encode(s"$u:$p".getBytes(Charsets.Utf8))
+    req.authorization = s"Basic $auth"
+    backend(req)
+  }
 
 }
