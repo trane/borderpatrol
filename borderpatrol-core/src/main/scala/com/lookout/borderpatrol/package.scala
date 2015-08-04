68ca2b62bf5cd80c3caf6f8395e1995a44f03cea
Andrew Kuhnhausen <andrew.kuhnhausen@lookout.com>
2015-08-04 10:05:06 -0700

    Improve scaladoc resolution of libraries
    Scaladoc was having trouble resolving some external dependencies in the
    comments, by formatting the dependencies as [[org..Package Package]]
    it will help with #2

--- borderpatrol-core/src/main/scala/com/lookout/borderpatrol/package.scala
+++ borderpatrol-core/src/main/scala/com/lookout/borderpatrol/package.scala
@@ -4,15 +4,47 @@ import com.twitter.util.Future
 import org.jboss.netty.handler.codec.http.HttpHeaders
 import com.twitter.finagle.httpx
 
+/**
+ * This is the root package of borderpatrol-core which provides a functional approach to web sessions and
+ * authentication built on top of [[com.twitter.finagle Finagle]]. It contains two main packages:
+ * [[com.lookout.borderpatrol.sessionx]] and [[com.lookout.borderpatrol.auth]] which contain types and functions to
+ * interact with HTTP services.
+ *
+ *
+ */
 package object borderpatrol {
   import auth._
   import sessionx._
 
+  /**
+   * Wraps any object with a `toFuture` method
+   *
+   * @param any object to be altered
+   *
+   * @tparam A object type
+   */
   implicit class AnyOps[A](val any: A) extends AnyVal {
-    def toFuture: Future[A] =
-      Future.value[A](any)
-    def toFailedFuture(e: Throwable): Future[A] =
-      Future.exception(e)
+
+    /**
+     * Wraps object into `Future`
+     * @return
+     */
+    def toFuture: Future[A] = Future.value[A](any)
+  }
+
+  /**
+   *
+   * Wraps any `Throwable` with a `toFutureException` method
+   * @param t throwable to wrap
+   */
+  implicit class ThrowableOps(val t: Throwable) extends AnyVal {
+
+    /**
+     * Wraps `Throwable` in a `Future` exception
+     * @tparam A
+     * @return
+     */
+    def toFutureException[A]: Future[A] = Future.exception[A](t)
   }
 
   implicit class IdxByteSeqOps(val bytes: IndexedSeq[Byte]) extends AnyVal {
@@ -25,17 +57,41 @@ package object borderpatrol {
       f(s)
   }
 
+  /**
+   * Contains the trait and companion object for explicit "views" from `A => B`
+   */
   object view {
 
+    /**
+     * Express transformations explicitly as some view from `A` to `B`, since Scala's "view" bounds are deprecated
+     */
     trait View[A, B] {
       def apply(a: A): B
     }
 
+    /**
+     * Helper object for creating Views
+     */
     object View {
+
+      /**
+       * Create a View[A,B] with a function from `A => B`
+       *
+       * @param f transformation that should succeed
+       * @tparam A from type
+       * @tparam B to type
+       * @return a view from A to B
+       */
       def apply[A, B](f: A => B): View[A, B] = new View[A, B] {
         def apply(a: A): B =
           f(a)
       }
+
+      /**
+       * Handles when a View[A, A] is created
+       * @tparam A
+       * @return
+       */
       implicit def identity[A]: View[A, A] =
         View(a => a)
     }
@@ -43,6 +99,8 @@ package object borderpatrol {
   }
 
   import view._
+
+  /** shorthand notation Thing %> Other */
   type %>[A, B] = View[A, B]
 
 
