e48ebc5075e79f7796045997bf5e2c9492c79616
Mahesh V Kelkar <Mahesh.Kelkar@lookout.com>
2015-10-09 17:12:00 -0400

    Fixed the test text for Tokens and added a test for failure to decode

--- auth/src/test/scala/com/lookout/borderpatrol/auth/keymaster/TokensSpec.scala
+++ auth/src/test/scala/com/lookout/borderpatrol/auth/keymaster/TokensSpec.scala
@@ -2,7 +2,8 @@ package com.lookout.borderpatrol.auth.keymaster
 
 import com.lookout.borderpatrol.sessionx._
 import com.lookout.borderpatrol.test.BorderPatrolSuite
-import scala.util.{Success, Try}
+import com.twitter.io.Buf
+import scala.util.{Try, Success}
 
 class TokensSpec extends BorderPatrolSuite  {
   import Tokens._
@@ -16,28 +17,27 @@ class TokensSpec extends BorderPatrolSuite  {
 
   behavior of "ServiceTokens"
 
-  it should "Verify the ServiceTokens methods" in {
+  it should "be able to find ServiceToken by service name" in {
     serviceTokens.find("service1") should be equals (serviceToken1)
     serviceTokens.find("service2") should be equals (serviceToken2)
   }
 
-  it should "Verify that encode and decode for ServiceTokens" in {
+  it should "uphold encoding/decoding ServiceTokens" in {
     def encodeDecode(tokens: ServiceTokens) : ServiceTokens = {
       ServiceTokensDecoder.decodeJson(ServiceTokensEncoder(tokens)).fold[ServiceTokens](e => ServiceTokens(), t => t)
     }
-
     encodeDecode(serviceTokens) should be (serviceTokens)
   }
 
   behavior of "Tokens"
 
-  it should "Verify the Tokens methods" in {
+  it should "be able to find the ServiceToken by service name" in {
     tokens.service("service1") should be equals (serviceToken1)
     tokens.service("service2") should be equals (serviceToken2)
     tokens.service("service3") should be equals (None)
   }
 
-  it should "Verify the encode and decode for Tokens" in {
+  it should "uphold encoding/decoding Tokens" in {
     def encodeDecode(toks: Tokens) : Tokens =
       TokensDecoder.decodeJson(TokensEncoder(toks)).fold[Tokens](e => emptyTokens, t => t)
     encodeDecode(tokens) should be (tokens)
@@ -45,12 +45,18 @@ class TokensSpec extends BorderPatrolSuite  {
 
   behavior of "SessionDataTokenEncoder"
 
-  it should "Verify encode and decode" in {
-    def validate[A](s: Tokens)(implicit ev: SessionDataEncoder[Tokens]): Try[Tokens] = {
+  it should "uphold encoding/decoding for SessionDataTokenEncoder" in {
+    def validate(s: Tokens)(implicit ev: SessionDataEncoder[Tokens]): Try[Tokens] = {
       ev.decode(ev.encode(s))
     }
-
     // Validate
     validate(tokens) should be (Success(tokens))
+  }
+
+  it should "not decode invalid data" in {
+    def invalid(input: Buf)(implicit ev: SessionDataEncoder[Tokens]): Try[Tokens] =
+      ev.decode(input)
+
+    invalid(SessionDataEncoder.encodeString.encode( """{ "a" : "b" }""")).failure.exception should be(a[SessionDataError])
   }
 }
