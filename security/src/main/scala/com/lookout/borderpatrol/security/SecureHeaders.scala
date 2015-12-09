360a26d047e61bd846e643838e18d85e150af39e
Andrew Kuhnhausen <andrew.kuhnhausen@lookout.com>
2015-12-09 15:37:58 -0800

    Ensure response and request headers are set
    I originally thought all of these were request headers, my bad -- this
    is why we can't have nice things. However, now I have fixed this and
    updated the tests.

--- security/src/main/scala/com/lookout/borderpatrol/security/SecureHeaders.scala
+++ security/src/main/scala/com/lookout/borderpatrol/security/SecureHeaders.scala
@@ -10,7 +10,9 @@ import com.lookout.borderpatrol.util.Combinators.tap
 /**
   * Basic, default values for adding security mechanisms via headers
   */
-object Headers {
+object SecureHeaders {
+
+  // response headers https://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Response_fields
   val StrictTransportSecurity = ("Strict-Transport-Security", "max-age=31557600")
   val XFrameOptions = ("X-Frame-Options", "DENY")
   val XXSSProtection = ("X-XSS-Protection", "1; mode=block")
@@ -18,34 +20,41 @@ object Headers {
   val XDownloadOptions = ("X-Download-Options", "noopen")
   val XPermittedCrossDomainPolicies = ("X-Permitted-Cross-Domain-Policies", "none")
 
-  val all = HeaderMap(StrictTransportSecurity, XFrameOptions, XXSSProtection,
-                      XContentTypeOptions, XDownloadOptions, XPermittedCrossDomainPolicies)
+  val response = HeaderMap(StrictTransportSecurity, XFrameOptions, XXSSProtection,
+                           XContentTypeOptions, XDownloadOptions, XPermittedCrossDomainPolicies)
+  val request = HeaderMap()
 }
 
 /**
-  * Inject specific headers into requests for added security or auditing
+  * Inject specific headers into requests and responses for added security
   *
   * By default it includes the following default headers and values:
   *
+  *   Responses:
   *   Strict-Transport-Security: max-age=31557600
   *   X-Frame-Options: DENY
   *   X-XSS-Protection: 1; mode=block
   *   X-ContentType-Options: nosniff
   *   X-Download-Options: noopen
   *   X-Permitted-Cross-Domain-Policies: none
+  *
+  *   Requests:
   *   X-Forwarded-For: the ip of this host appended to any existing values
   *
-  * @param headers
+  * @param requestHeaders
+  * @param responseHeaders
   */
-case class HeaderFilter(headers: HeaderMap = Headers.all) extends SimpleFilter[Request, Response] {
+case class SecureHeaderFilter(requestHeaders: HeaderMap = SecureHeaders.request,
+                              responseHeaders: HeaderMap = SecureHeaders.response)
+    extends SimpleFilter[Request, Response] {
   val localIp = InetAddress.getLocalHost.getHostAddress
 
   def apply(req: Request, service: Service[Request, Response]): Future[Response] =
     service(tap(req) { r =>
-        r.xForwardedFor = (r.xForwardedFor match {
-          case Some(s) => s"$s, $localIp"
-          case None => localIp
-        })
-      r.headerMap ++= headers
-    })
+      r.xForwardedFor = (r.xForwardedFor match {
+        case Some(s) => s"$s, $localIp"
+        case None => localIp
+      })
+      r.headerMap ++= requestHeaders
+    }).map(r => tap(r)(_.headerMap ++= responseHeaders))
 }
---
+++ security/src/test/scala/com/lookout/borderpatrol/test/security/SecureHeadersSpec.scala
@@ -0,0 +1,43 @@
+package com.lookout.borderpatrol.test.security
+
+import com.lookout.borderpatrol.security.SecureHeaderFilter
+import com.lookout.borderpatrol.security.SecureHeaders
+import com.lookout.borderpatrol.test._
+import com.twitter.finagle.Service
+import com.twitter.finagle.httpx.{Response, Request, Status}
+import com.twitter.util.Future
+
+class SecureHeadersSpec extends BorderPatrolSuite {
+
+  behavior of "SecureHeaderFilter"
+  val defaults = SecureHeaders.response
+  val filter = SecureHeaderFilter(defaults)
+  val service = filter andThen testService(r => true)
+
+  it should "inject all of the headers" in {
+    val request = Request("/")
+    service(request).results.headerMap.sameElements(defaults) should be(true)
+  }
+
+  it should "append X-Forwarded-For to an existing list into request" in {
+    val request = Request("/")
+    request.xForwardedFor = "10.10.10.10"
+
+    filter(request, testService(r =>
+      r.xForwardedFor.getOrElse("").split(",").size > 1)
+    ).results.status should be (Status.Ok)
+
+    // empty xforwardedfor
+    filter(Request("/"), testService(r =>
+      r.xForwardedFor.getOrElse("").split(",").size == 1)
+    ).results.status should be (Status.Ok)
+  }
+
+  it should "override existing headers" in {
+    val request = Request("/")
+    val response = Response(Status.Ok)
+    response.headerMap.add("X-Download-Options", "arglebargle")
+    val s = Service.mk[Request, Response](r => Future.value(response))
+    filter(request, s).results.headerMap.sameElements(defaults) should be(true)
+  }
+}
